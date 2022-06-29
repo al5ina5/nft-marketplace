024cd18986c33b1aed56fe01337b817565079aa5
Nicholas Keller <nkeller@profilelag.org>
2022-06-29 10:43:49 -0400

    PR attempt 1

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,7 +1,11 @@
-import Link from "next/link"
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import useSWR from "swr"
-import { imageUrl } from "../../utils/utils"
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-useless-path-segments */
+/* eslint-disable import/extensions */
+/* eslint-disable react/jsx-one-expression-per-line */
+import Link from 'next/link'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import useSWR from 'swr'
+import { imageUrl } from './../../utils/utils'
 
 export default function CollectionsIndex() {
     const { data: collections } = useSWR('/collections')
@@ -10,24 +14,23 @@ export default function CollectionsIndex() {
 
     return (
         <div className="p-6 py-12 max-w-7xl mx-auto space-y-12 w-full">
-            {collections && <ResponsiveMasonry
-                columnsCountBreakPoints={{ 350: largeGrid ? 1 : 2, 750: largeGrid ? 2 : 4, 900: largeGrid ? 3 : 5 }}
-            >
-                <Masonry
-                    gutter="2em"
-                >{collections.map(collection =>
-                    <Link href={collection.contractAddress}>
-                        <a className="bg-white rounded overflow-hidden transform transition hover:-translate-y-2">
-                            <img loading="lazy" className="w-full" src={imageUrl(collection.image[0])} alt="" />
-                            <div className="p-6 text-center">
-                                <p className="text-zinc-900 truncate uppercase font-extended text-xl">{collection.name}</p>
-                                <p>{collection.totalSupply} Items</p>
-                            </div>
-                        </a>
-                    </Link>
-                )}
-                </Masonry>
-            </ResponsiveMasonry>}
+            {collections && (
+                <ResponsiveMasonry columnsCountBreakPoints={{ 350: largeGrid ? 1 : 2, 750: largeGrid ? 2 : 4, 900: largeGrid ? 3 : 5 }}>
+                    <Masonry gutter="2em">
+                        {collections.map((collection) => (
+                            <Link href={collection.contractAddress}>
+                                <a className="bg-white rounded overflow-hidden transform transition hover:-translate-y-2">
+                                    <img loading="lazy" className="w-full" src={imageUrl(collection.image[0])} alt="" />
+                                    <div className="p-6 text-center">
+                                        <p className="text-zinc-900 truncate uppercase font-extended text-xl">{collection.name}</p>
+                                        <p>{collection.totalSupply} Items</p>
+                                    </div>
+                                </a>
+                            </Link>
+                        ))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            )}
         </div>
     )
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,22 +1,26 @@
+/* eslint-disable no-param-reassign */
 import Web3 from 'web3'
 import commaNumber from 'comma-number'
 import shortNumber from 'short-number'
 import axios from 'axios'
 import { mutate } from 'swr'
 
-// export const apiUrl = 'https://api.fantom.digital'
-export const apiUrl = process.env.NODE_ENV === 'production' ? 'https://api.fantom.digital' : 'http://localhost:8888'
+export const apiUrl = 'https://api.fantom.digital'
+// export const apiUrl = process.env.NODE_ENV === 'production' ? 'https://api.fantom.digital' : 'http://localhost:8888'
 export const api = axios.create({ baseURL: apiUrl })
 
 export const imageCacheUrl = 'https://enbzwejmbbmiirvimcib.supabase.in/storage/v1/object/public/cache'
 export const ipfsGateway = 'https://ftmdead.mypinata.cloud/ipfs/'
-export const imageUrl = (string = "") => {
-    string = string.replace('ipfs://', ipfsGateway)
-    string = string.replace('https://ipfs.io/ipfs/', ipfsGateway)
-    string = string.replace('https://ipfs.io/ipfs/', ipfsGateway)
-    string = string.replace('https://gateway.pinata.cloud/ipfs/', ipfsGateway)
-    return `${string}`
-    // return `http://nft-marketplace-optimizer.alsina.xyz/image/${string}`
+export const imageUrl = (url = '') => {
+    if (!url) return ''
+    url = url.replace('ipfs://', ipfsGateway)
+    url = url.replace('https://ipfs.io/ipfs/', ipfsGateway)
+    url = url.replace('https://ipfs.io/ipfs/', ipfsGateway)
+    url = url.replace('https://gateway.pinata.cloud/ipfs/', ipfsGateway);
+    (async () => {
+        url = (await axios.get(`http://nft-marketplace-optimizer.alsina.xyz/image/${url}`)).data
+    })()
+    return url
 }
 
 export const indexCollection = async (contractAddress: string) => {
@@ -28,4 +32,3 @@ export const indexCollection = async (contractAddress: string) => {
 export const cacheImage = (url: string) => `${apiUrl}/cache/${encodeURIComponent(url)}`
 // export const cacheImage = (url: string) => url
 export const formatPricing = (price) => `${Number(price) > 10000 ? shortNumber(Number(Web3.utils.fromWei(price))) : commaNumber(Number(Web3.utils.fromWei(price)))} FTM`
-
