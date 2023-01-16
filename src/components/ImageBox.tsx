66755811aa864780c536fdfbccad82e9583c3451
Sebastian Alsina <alsinas@me.com>
2023-01-16 02:19:42 -0500

    Fixed smol error

--- src/components/ImageBox.tsx
+++ src/components/ImageBox.tsx
@@ -16,7 +16,7 @@ export default function ImageBox({ nft }) {
                     <a className={classNames("group relative bg-white bg-opacity-5 border border-white border-opacity-10 rounded overflow-hidden flex items-center justify-center", error && 'bg-red-500 border-red-500', !error && !loaded && 'animate-pulse')} style={{ minHeight: '8em' }}>
                         <div className='absolute inset-0 z-20 bg-black bg-opacity-25 opacity-0 group-hover:opacity-100 transition pointer-events-none'>
                             <div className='p-4 flex items-start justify-start gap-4 flex-wrap'>
-                                <p className='bg-white text-black p-2'>{nft.metadata.name}</p>
+                                <p className='bg-white text-black p-2'>{nft?.metadata?.name}</p>
                                 {nft?.listing && <><p className='bg-white text-black p-2'>{nft?.listing?.status === 'listed' ? `${Web3.utils.fromWei(nft?.listing?.price)} FTM` : nft?.listing?.status}</p></>}
                             </div>
                         </div>
